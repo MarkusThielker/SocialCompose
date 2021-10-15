f87c88cd7325455034a25b6d81cf77e16df9edde
Markus Thielker <mail.markus.thielker@gmail.com>
2021-10-15 15:30:25 +0200

    test(server): Add tests for authentication and registration

---
+++ server/src/test/kotlin/com/example/social_compose/server/AuthenticationTest.kt
@@ -0,0 +1,85 @@
+package com.example.social_compose.server
+
+import com.example.social_compose.server.plugins.configureDatabase
+import com.example.social_compose.server.plugins.configureSecurity
+import com.example.social_compose.shared.AuthCredentials
+import com.example.social_compose.shared.AuthToken
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@ExperimentalSerializationApi
+class AuthenticationTest {
+
+    @Test
+    fun testAuthenticationSuccess() {
+
+        var callResponse: String
+
+        val credentialsJson = Json.encodeToString(
+            AuthCredentials.serializer(), AuthCredentials("MarkusT", "password")
+        )
+
+        val timestampPattern = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d*"
+        val responsePattern = "Hello, [\\w]{3,16}! Your token was issued at $timestampPattern" +
+                " and will expire in \\d* minutes and \\d* seconds"
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/authenticate") {
+                setBody(credentialsJson)
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                callResponse = Json.decodeFromString<AuthToken>(response.content.toString()).token
+                assert(callResponse.matches(Regex("\\S+.\\S+.\\S+")))
+            }
+
+            handleRequest(HttpMethod.Get, "/api/v1/user/token-status") {
+                addHeader("Authorization", "Bearer $callResponse")
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assert(response.content.toString().matches(Regex(responsePattern)))
+            }
+        }
+    }
+
+    @Test
+    fun testAuthenticationFailedUsername() {
+
+        val credentialsJson = Json.encodeToString(
+            AuthCredentials.serializer(), AuthCredentials("NotMarkusT", "password")
+        )
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/authenticate") {
+                setBody(credentialsJson)
+            }.apply {
+                assertEquals(HttpStatusCode.Unauthorized, response.status())
+                assertEquals(response.content, "Username not found")
+            }
+        }
+    }
+
+    @Test
+    fun testAuthenticationFailedPassword() {
+
+        val credentialsJson = Json.encodeToString(
+            AuthCredentials.serializer(), AuthCredentials("MarkusT", "NotThePassword")
+        )
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/authenticate") {
+                setBody(credentialsJson)
+            }.apply {
+                assertEquals(HttpStatusCode.Unauthorized, response.status())
+                assertEquals(response.content, "Password not correct")
+            }
+        }
+    }
+}
---
+++ server/src/test/kotlin/com/example/social_compose/server/RegistrationTest.kt
@@ -0,0 +1,150 @@
+package com.example.social_compose.server
+
+import com.example.social_compose.server.plugins.configureDatabase
+import com.example.social_compose.server.plugins.configureSecurity
+import com.example.social_compose.shared.user.UserRegistration
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@ExperimentalSerializationApi
+class RegistrationTest {
+
+    @Test
+    fun testRegistrationFailedDuplicateUsername() {
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            val userRegistration = UserRegistration(
+                "MarkusT",
+                "null@null.com",
+                "password",
+                "alias",
+            )
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/create") {
+                setBody(Json.encodeToString(userRegistration))
+            }.apply {
+                assertEquals(HttpStatusCode.Conflict, response.status())
+                assertEquals("Username is already used by another account", response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRegistrationFailedDuplicateEmail() {
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            val userRegistration = UserRegistration(
+                "username",
+                "mail@example.com",
+                "password",
+                "alias",
+            )
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/create") {
+                setBody(Json.encodeToString(userRegistration))
+            }.apply {
+                assertEquals(HttpStatusCode.Conflict, response.status())
+                assertEquals("Email is already used by another account", response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRegistrationFailedFormatUsername() {
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            val userRegistration = UserRegistration(
+                "usernameIsTooLong",
+                "null@null.com",
+                "password",
+                "alias",
+            )
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/create") {
+                setBody(Json.encodeToString(userRegistration))
+            }.apply {
+                assertEquals(HttpStatusCode.Conflict, response.status())
+                assertEquals(
+                    "The data sent for registration doesn't match the format requirements", response.content
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testRegistrationFailedFormatEmail() {
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            val userRegistration = UserRegistration(
+                "username",
+                "invalidEmail",
+                "password",
+                "alias",
+            )
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/create") {
+                setBody(Json.encodeToString(userRegistration))
+            }.apply {
+                assertEquals(HttpStatusCode.Conflict, response.status())
+                assertEquals(
+                    "The data sent for registration doesn't match the format requirements", response.content
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testRegistrationFailedFormatPassword() {
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            val userRegistration = UserRegistration(
+                "username",
+                "null@null.com",
+                "short",
+                "alias",
+            )
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/create") {
+                setBody(Json.encodeToString(userRegistration))
+            }.apply {
+                assertEquals(HttpStatusCode.Conflict, response.status())
+                assertEquals(
+                    "The data sent for registration doesn't match the format requirements", response.content
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testRegistrationFailedFormatAlias() {
+
+        withTestApplication({ configureDatabase(); configureSecurity() }) {
+
+            val userRegistration = UserRegistration(
+                "username",
+                "null@null.com",
+                "password",
+                "", // missing alias
+            )
+
+            handleRequest(HttpMethod.Post, "/api/v1/user/create") {
+                setBody(Json.encodeToString(userRegistration))
+            }.apply {
+                assertEquals(HttpStatusCode.Conflict, response.status())
+                assertEquals(
+                    "The data sent for registration doesn't match the format requirements", response.content
+                )
+            }
+        }
+    }
+}
