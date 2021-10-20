bbd169574a7d182b6c6e0ab508fc2f6ec92dc22e
Markus Thielker <mail.markus.thielker@gmail.com>
2021-10-20 13:24:06 +0200

    feat(server): Add call logging and location plugin

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-locations:$ktorVersion")
     implementation("io.ktor:ktor-serialization:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
--- server/src/main/kotlin/com/example/social_compose/server/Application.kt
+++ server/src/main/kotlin/com/example/social_compose/server/Application.kt
@@ -6,11 +6,15 @@ import com.example.social_compose.server.plugins.configureDatabase
 import com.example.social_compose.server.plugins.configureSecurity
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.locations.*
+import io.ktor.request.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import org.slf4j.event.Level
 
+@KtorExperimentalLocationsAPI
 @ExperimentalSerializationApi
 fun main(args: Array<String>) {
 
@@ -22,6 +26,19 @@ fun main(args: Array<String>) {
                 isLenient = true
             }
         }
+
+        install(CallLogging) {
+            level = Level.INFO
+
+            format { call ->
+                val route = call.request.uri
+                val httpMethod = call.request.httpMethod.value
+                val status = call.response.status()
+                "Route: $route, HTTP method: $httpMethod, Status: $status"
+            }
+        }
+
+        install(Locations)
 
         configureDatabase()
         configureSecurity()
